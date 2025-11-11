9d45c0b66cd768ab81ea661eeb8ba6c94ed6bcd6
Philipp Ochs <ufiiu@student.kit.edu>
2025-11-11 01:14:58 +0100

    Implemented Uuid().v4() for userID
    Unique User id with Uuidv4

--- open_wearable/lib/apps/YML_tracker/model/Experiment/Participant.dart
+++ open_wearable/lib/apps/YML_tracker/model/Experiment/Participant.dart
@@ -1,14 +1,10 @@
 class Participant {
   final String id;
 
+  Participant._(this.id);
 
-  //Eindeutige ID Zuweisung
   factory Participant.generate() {
-    final id = _generateRadomID();
-  }
-
-  static String _generateRandomID() {
-    //mybe eeher nicht random, sondern eher per uuidv4 sonst kriese mit Dsgvo kram
+    return Participant._(Uuid().v4());
   }
 
   @Override
